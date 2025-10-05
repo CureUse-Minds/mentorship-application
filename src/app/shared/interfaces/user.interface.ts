f3adc74b556867f4eb5580446009076bb87bea24
Alexis Dela Torre <alexisndelatorre@gmail.com>
2025-10-05 23:21:03 +0800

    modify userRole type; Will create  a separate interface for admin

--- src/app/shared/interfaces/user.interface.ts
+++ src/app/shared/interfaces/user.interface.ts
@@ -1,11 +1,13 @@
+export type UserRole = 'mentor' | 'mentee';
 export interface User {
   id: string;
   email: string;
   firstName: string;
   lastName: string;
-  role: 'mentor' | 'mentee' | 'admin';
+  role: UserRole;
   profilePicture?: string;
   bio?: string;
   createdAt: Date;
   updatedAt: Date;
+  emailVerified?: boolean;
 }

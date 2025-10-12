50f05bb06d93815094f2dde0b3e9ac2d92664dd0
Joy Dimaculangan <119020814+joyreyesdimaculangan@users.noreply.github.com>
2025-10-12 11:05:56 +0800

    Delete Firebase config from production environment
    Remove production environment configuration for Firebase.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,1 @@
-export const environment = {
-  production: true,
-  firebaseConfig: {
-    apiKey: 'AIzaSyBcTMWDws60YTNp8Dv-h90RGXdgmSUP9QU',
-    authDomain: 'mentorship-app-angular.firebaseapp.com',
-    projectId: 'mentorship-app-angular',
-    storageBucket: 'mentorship-app-angular.firebasestorage.app',
-    messagingSenderId: '510740484701',
-    appId: '1:510740484701:web:81ae056c2486e11aedb3b4',
-    measurementId: 'G-08K2DSE23K',
-  },
-};
+

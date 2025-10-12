381f83475c7e5d5da09d26ebcc3d8a9e7d9f47aa
Joy Dimaculangan <119020814+joyreyesdimaculangan@users.noreply.github.com>
2025-10-12 11:04:32 +0800

    Remove sensitive configuration from environment.ts
    Removed Firebase and Google Calendar configuration from environment file.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,1 @@
-export const environment = {
-  production: false,
-  firebaseConfig: {
-    apiKey: 'AIzaSyBcTMWDws60YTNp8Dv-h90RGXdgmSUP9QU',
-    authDomain: 'mentorship-app-angular.firebaseapp.com',
-    projectId: 'mentorship-app-angular',
-    storageBucket: 'mentorship-app-angular.firebasestorage.app',
-    messagingSenderId: '510740484701',
-    appId: '1:510740484701:web:81ae056c2486e11aedb3b4',
-    measurementId: 'G-08K2DSE23K',
-  },
-  googleCalendar: {
-    apiKey: 'AIzaSyDX7z8qId6UuF6et6WKZjpHs9e6t53F4xo',
-    clientId: '834951469188-fp0b0vm6lfagbmi8dfnq03vlcpl5dtug.apps.googleusercontent.com'
-  }
-};
+
